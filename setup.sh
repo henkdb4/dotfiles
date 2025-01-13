a231605cbe1c12b8eee84c0ec8c7770c8777a03f
TornHeckr <henkdb4@pm.me>
2025-01-13 23:50:00 +0100

    Changed setup.d to scripts for my tabsanity

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 scriptdir="$(dirname -- $0)"
-setups="$(find $scriptdir/setup.d/ -maxdepth 1 -mindepth 1 -executable)"
+setups="$(find $scriptdir/scripts/ -maxdepth 1 -mindepth 1 -executable)"
 
 for setup in $setups; do
 	echo "Running setup: $setup"
