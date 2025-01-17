c7240a2e9e14199ddfb74f31206d1efe9dc6fdae
TornHeckr <henkdb4@pm.me>
2025-01-17 21:45:37 +0100

    Added exit code to logging

--- setup.sh
+++ setup.sh
@@ -6,6 +6,6 @@ setups="$(find $scriptdir/scripts/ -maxdepth 1 -mindepth 1 -executable)"
 for setup in $setups; do
 	echo "Running setup: $setup"
 	"$setup"
-	echo "FInnished running setup: $setup"
+	echo "Finished running $setup with exit code $?"
 done
 
