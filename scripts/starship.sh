3d59f265adcfb339754ed0166cad750717025f4c
TornHeckr <henkdb4@pm.me>
2025-01-17 21:21:46 +0100

    Added starship to scripts

---
+++ scripts/starship.sh
@@ -0,0 +1,10 @@
+#!/bin/env bash
+
+if [ -n $TERMUX_VERSION ]; then
+  echo "Running termux, installing via pkg"
+  pkg i starship
+  exit 0;
+fi
+
+echo "Installing starship via remote installer..."
+curl -sS https://starship.rs/install.sh | sh
--- scripts/termux.sh
+++ scripts/termux.sh
@@ -5,4 +5,4 @@ if [ -z $TERMUX_VERSION ]; then
   exit 0;
 fi
 
-pkg i topgrade starship neofetch which git openssh neovim elvish zig ncdu fdupes ranger stow wget man jq htop figlet
+pkg i topgrade neofetch which git openssh neovim elvish zig ncdu fdupes ranger stow wget man jq htop figlet
