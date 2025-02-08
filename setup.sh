a8d7a16be34a086d5972dae7bbc0ce1426005285
TornHeckr <henkdb4@pm.me>
2025-02-08 15:29:24 +0100

    move to setup folder before starting to run scripts

--- setup.sh
+++ setup.sh
@@ -3,9 +3,10 @@
 scriptdir="$(dirname -- $0)"
 setups="$(find $scriptdir/scripts/ -maxdepth 1 -mindepth 1 -executable | sort)"
 
+pushd $scriptdir
 for setup in $setups; do
 	echo "Running setup: $setup"
 	"$setup"
   echo "Finished running $(basename $setup) with exit code $?"
 done
-
+popd
