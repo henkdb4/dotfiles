c6d1d4e90ef7122f7daec7b26ed1b6bd4732dd0e
TornHeckr <henkdb4@pm.me>
2025-03-20 19:22:43 +0100

    Added flatpak to packages directory

--- scripts/flatpak.sh
+++ scripts/flatpak.sh
@@ -5,14 +5,10 @@ if ! command -v flatpak; then
   exit 0;
 fi
 
-pkgs="com.discordapp.Discord
-  com.spotify.Client
-  md.obsidian.Obsidian"
-
 echo "Adding Repos to flatpak"
 flatpak remote-add --if-not-exists --system flathub https://dl.flathub.org/repo/flathub.flatpakrepo
 
 echo "Installing fatpak apps"
-for pkg in $pkgs; do
+for pkg in $(<pkgs/flatpak); do
   flatpak install --system flathub $pkg
 done
