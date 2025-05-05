5103565a5d6f4c4fc668afaf307f8a1e322550b2
TornHeckr <henkdb4@pm.me>
2025-05-05 19:48:28 +0200

    Added micropython to nvim

--- scripts/termux.sh
+++ scripts/termux.sh
@@ -5,6 +5,6 @@ if [ -z $TERMUX_VERSION ]; then
   exit 0;
 fi
 
-pkgs="$(<pkgs/default)"
+pkgs="$(<pkgs/default) $(<pkgs/shell)"
 
 pkg i topgrade which openssh neovim elvish zig ranger wget man jq htop $pkgs
