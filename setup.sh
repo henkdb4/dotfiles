52d3eafd283e70710f85584fe326a6c8c6ec494a
TornHeckr <henkdb4@pm.me>
2025-08-07 21:19:27 +0200

    Added error messages

--- setup.sh
+++ setup.sh
@@ -5,8 +5,16 @@ setups="$(find $scriptdir/scripts/ -maxdepth 1 -mindepth 1 -executable | sort)"
 
 pushd $scriptdir
 for setup in $setups; do
-	echo "Running setup: $setup"
+	printf "\e[0;36mRunning setup: $setup\e[0;37m\n"
 	"$setup"
-  echo "Finished running $(basename $setup) with exit code $?"
+	result=$?
+
+	if [[ $result -eq 0 ]]; then
+		printf "\e[0;34mFinished running $(basename $setup) with exit code $result\e[0;37m\n\n"
+	elif [[ $result -eq 255 ]]; then
+		printf "\e[0;33mSkipped $(basename $setup) with exit code $result\e[0;37m\n\n"
+	else 
+		printf "\e[0;31mFailed $(basename $setup) with exit code $result\e[0;37m\n\n"
+	fi
 done
 popd
