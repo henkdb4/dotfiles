d7c14d8188ba7822abc7262896f66f55804ad8c4
TornHeckr <henkdb4@pm.me>
2025-08-09 14:23:42 +0200

    Remove pre repo upgrade to prevent sudo when not needed

--- scripts/arch.sh
+++ scripts/arch.sh
@@ -7,13 +7,11 @@ fi
 
 PackageList="neovim luarocks man $(<pkgs/default) $(<pkgs/shell) $(<pkgs/wm) $(<pkgs/apps)"
 
-yay -Sy --noconfirm # I sometimes have problems with an outdated database
-
 for pkg in $PackageList 
 do
   if [[ -z $(yay -Q $pkg) ]]
   then 
     echo "Installing $pkg..."
-    yay -S --noconfirm $pkg
+    yay -Sy --noconfirm $pkg
   fi
 done
